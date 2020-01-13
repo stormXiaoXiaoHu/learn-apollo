5c156c72937c4eda12f1b0be781e5f07a39a029c
hujie <stormXiaoXiaoHu@163.com>
2020-01-13 16:16:46 +0800

    Apollo源码解析~~Portal侧之AppController

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/controller/AppController.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/controller/AppController.java
@@ -52,6 +52,9 @@ public class AppController {
   private final UserInfoHolder userInfoHolder;
   private final AppService appService;
   private final PortalSettings portalSettings;
+  /**
+   * Spring 事件发布者
+   */
   private final ApplicationEventPublisher publisher;
   private final RolePermissionService rolePermissionService;
   private final RoleInitializationService roleInitializationService;
@@ -107,23 +110,29 @@ public class AppController {
     return appService.findByAppIds(appIds, page);
   }
 
+  /**
+   * 创建App
+   *
+   * @param appModel AppModel对象
+   * @return App 对象
+   */
   @PreAuthorize(value = "@permissionValidator.hasCreateApplicationPermission()")
   @PostMapping
   public App create(@Valid @RequestBody AppModel appModel) {
-
+    // 将 AppModel 转换成 App 对象
     App app = transformToApp(appModel);
-
+    // 保存 App 对象到数据库
     App createdApp = appService.createAppInLocal(app);
-
+    // 发布 AppCreationEvent 创建事件
     publisher.publishEvent(new AppCreationEvent(createdApp));
-
+    // 授予 App 管理员的角色
     Set<String> admins = appModel.getAdmins();
     if (!CollectionUtils.isEmpty(admins)) {
       rolePermissionService
           .assignRoleToUsers(RoleUtils.buildAppMasterRoleName(createdApp.getAppId()),
               admins, userInfoHolder.getUser().getUserId());
     }
-
+    // 返回 App 对象
     return createdApp;
   }
 
